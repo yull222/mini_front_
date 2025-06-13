2980bf0305fb8eb1621ac7adc905b8b56b1888e1
ohyeon1002 <skyblueazure4@gmail.com>
2025-06-13 14:55:05 +0900

    scrap

--- src/pages/Scrap.tsx
+++ src/pages/Scrap.tsx
@@ -1,40 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-type newsHistory = {
-  title: string;
-  originallink: string;
-  link: string;
-  summary: string;
-  pubDate: string;
+type fav = {
+  items: {
+    title: string;
+    link: string;
+    summary: string;
+    pubDate: string;
+  }[]
 };
 
 export default function Scrap() {
-  const [news, setNews] = useState({});
+  const [news, setNews] = useState<fav>();
   const historyRetrieve = async (): Promise<void> => {
-    const resp: Response = await fetch("server.com/history", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    });
-    const jsn = await resp.json();
-    setNews(jsn.itmes);
+    try {
+      const resp: Response = await fetch("http://localhost:3000/items", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const jsn = await resp.json();
+      // console.log(jsn);
+      setNews(jsn);
+    } catch (e) {
+      console.error(e);
+    }
   };
-  if (news == {}){
-  return (
-    <div className="bg-white shadow-md rounded-lg p-4 m-2 hover:scale-105 transition-transform duration-200">
-      <a
-        href={news.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="block"
-      >
-        <h2 className="text-lg font-semibold text-gray-800 hover:text-blue-600 mb-2">
-          {news.title
-            .replace(/<b>/g, "")
-            .replace(/<\/b>/g, "")
-            .replace(/&quot;/g, '"')}
-        </h2>
-        <p className="text-sm text-gray-500">{news.pubDate}</p>
-      </a>
-    </div>
-  );}
+  useEffect(() => {
+    historyRetrieve();
+  }, []);
+  const tags = news
+    ? news.map((item) => (
+        <div className="bg-white shadow-md rounded-lg p-4 m-2 hover:scale-105 transition-transform duration-200" key={item.link}>
+          <a
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block"
+          >
+            <h2 className="text-lg font-semibold text-gray-800 hover:text-blue-600 mb-2">
+              {item.title
+                .replace(/<b>/g, "")
+                .replace(/<\/b>/g, "")
+                .replace(/&quot;/g, '"')}
+            </h2>
+            <p className="text-sm text-gray-500">{item.pubDate}</p>
+          </a>
+        </div>
+      ))
+    : "";
+  return <>{tags}</>;
 }
